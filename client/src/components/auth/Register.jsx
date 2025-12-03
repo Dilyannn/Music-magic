627ce91c15c659ab275279eb4d1bc6bf7f185d1e
Dilyannn <dilyanpaypal@gmail.com>
2025-12-03 14:43:29 +0200

    Refactor auth components and extract ButtonSvg
    Moved HeaderAuth.jsx to the 'others' subfolder and updated imports in Login.jsx and Register.jsx accordingly. Extracted the SVG button icon into a new reusable ButtonSvg component to reduce code duplication in Login and Register forms.

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import HeaderAuth from './HeaderAuth.jsx';
+
+import HeaderAuth from './others/HeaderAuth.jsx';
+import ButtonSvg from './others/ButtonSvg.jsx';
 
 const Register = () => {
   return (
@@ -53,11 +54,7 @@ const Register = () => {
                 type="submit"
                 className="group relative flex w-full justify-center rounded-lg bg-purple-600 px-4 py-3 text-sm font-semibold text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
               >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg className="h-5 w-5 text-purple-300 group-hover:text-purple-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
-                  </svg>
-                </span>
+                <ButtonSvg />
                 Register
               </button>
             </div>
