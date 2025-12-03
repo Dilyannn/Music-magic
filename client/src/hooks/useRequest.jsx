42e8c88b91a22f32bca17dff598b2299fca35bff
Dilyannn <dilyanpaypal@gmail.com>
2025-12-03 15:44:41 +0200

    Add custom hooks for form handling and API requests
    Introduces useForm for managing form state and actions, and useRequest for making authenticated API requests using context. These hooks improve code reuse and simplify form and request logic in components.

---
+++ client/src/hooks/useRequest.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
+
+export default function useRequest() {
+    const { user, isAuthenticated } = useContext(UserContext);
+
+    const request = async (url, method, data, config = {}) => {
+        let options = {};
+
+        if (method) {
+            options.method = method;
+        }
+
+        if (data) {
+            options.headers = {
+                "content-type": "application/json"
+            };
+
+            options.body = JSON.stringify(data);
+        }
+
+        if(config.accessToken || isAuthenticated) {
+            options.headers = {
+                ...options.headers,
+                "X-Authorization": config.accessToken || user.accessToken
+            }
+        }
+
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw response.statusText;
+        }
+
+        if(response.status === 204) {
+            return {}; 
+        }
+
+        const result = await response.json();
+
+        return (result);
+    };
+
+    return {
+        request
+    };
+}
